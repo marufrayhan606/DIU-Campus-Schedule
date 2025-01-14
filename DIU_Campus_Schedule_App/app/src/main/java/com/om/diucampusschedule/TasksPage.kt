1fd4fa572bba8fbc6d0cf7d943410ab800f07962
Maruf Rayhan <marufrayhan2002@gmail.com>
2025-01-14 11:58:14 +0600

    Fixed Bottom Navigation Bar to show in all screens

--- DIU_Campus_Schedule_App/app/src/main/java/com/om/diucampusschedule/TasksPage.kt
+++ DIU_Campus_Schedule_App/app/src/main/java/com/om/diucampusschedule/TasksPage.kt
@@ -1,9 +1,27 @@
 package com.om.diucampusschedule
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 
 @Composable
 fun TasksPage(navController: NavHostController){
     // This is the second page
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(text = "Tasks Page", style = MaterialTheme.typography.headlineMedium, color = MaterialTheme.colorScheme.primary, fontSize = 28.sp)
+
+    }
 }
